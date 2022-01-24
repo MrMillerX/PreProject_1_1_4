cb8edc5f834c7cae33bb4fa2a1e806f6324214a5
MrMillerX <1marcusmiller1@gmail.com>
2022-01-24 11:46:59 +0300

    Finished the task

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,32 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        // Создание таблицы User(ов)
+        // Добавление 4 User(ов) в таблицу с данными на свой выбор.
+        // После каждого добавления должен быть вывод в консоль ( User с именем – name добавлен в базу данных )
+        // Получение всех User из базы и вывод в консоль ( должен быть переопределен toString в классе User)
+        // Очистка таблицы User(ов)
+        // Удаление таблицы
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Ryan", "Gosling", (byte) 41);
+        System.out.println("User с именем – Ryan Gosling добавлен в базу данных");
+        userService.saveUser("Adam", "Driver", (byte) 38);
+        System.out.println("User с именем – Adam Driver добавлен в базу данных");
+        userService.saveUser("Cristin", "Milioti", (byte) 36);
+        System.out.println("User с именем – Cristin Milioti добавлен в базу данных");
+        List<User> userList = userService.getAllUsers();
+        for (User user : userList) {
+            System.out.println(user);
+        }
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,7 +1,10 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
@@ -10,26 +13,71 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void createUsersTable() {
-
+        try (Statement state = Util.getNewConnection().createStatement()) {
+            state.execute("CREATE TABLE IF NOT EXISTS users (" +
+                    "id BIGINT(19) NOT NULL AUTO_INCREMENT," +
+                    "name VARCHAR(45) NOT NULL," +
+                    "lastName VARCHAR(45) NOT NULL," +
+                    "age TINYINT(3) NOT NULL," +
+                    "PRIMARY KEY (id));");
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
     }
 
     public void dropUsersTable() {
-
+        try (Statement state = Util.getNewConnection().createStatement()) {
+            state.executeUpdate("DROP TABLE IF EXISTS Users");
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        try (PreparedStatement state = Util.getNewConnection()
+                .prepareStatement("INSERT INTO users(name, lastname, age) VALUES(?, ?, ?)")) {
+            state.setString(1, name);
+            state.setString(2, lastName);
+            state.setByte(3, age);
+            state.execute();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
     }
 
     public void removeUserById(long id) {
-
+        try (PreparedStatement state = Util.getNewConnection().prepareStatement("DELETE FROM users WHERE id = ?")) {
+            state.setLong(1, id);
+            state.execute();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
     }
 
     public List<User> getAllUsers() {
+        try (Statement state = Util.getNewConnection().createStatement()) {
+            List<User> userList = new ArrayList<>();
+            ResultSet resultUsers = state.executeQuery("SELECT * FROM users");
+            while (resultUsers.next()) {
+                User bufUser = new User();
+                bufUser.setId(resultUsers.getLong("id"));
+                bufUser.setName(resultUsers.getString("name"));
+                bufUser.setLastName(resultUsers.getString("lastname"));
+                bufUser.setAge(resultUsers.getByte("age"));
+                userList.add(bufUser);
+            }
+            return userList;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
         return null;
     }
 
     public void cleanUsersTable() {
-
+        try (Statement state = Util.getNewConnection().createStatement()) {
+            state.executeUpdate("DELETE FROM users");
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
     }
 }
